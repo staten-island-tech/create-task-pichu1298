49f070f03d8e2f2c7e17faedd79b9103cac80a7f
pichu1298 <62480135+pichu1298@users.noreply.github.com>
2024-12-12 12:32:36 -0500

    ads

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,18 +1,21 @@
 import { randomItems } from "./array";
+import "../style/style.css";
 
 const DomSelectors = {
   container: document.querySelector(".container"),
   buttonContainer: document.querySelector(".buttonContainer"),
 };
 
-DomSelectors.container.insertAdjacentHTML(
-  "beforeend",
-  randomItems.forEach(
-    (item) => `<div class = "card" id = "">
-    <h1 class = "name" id = "${item.name}">${item.name}</h1>
-    <h2 class = "${item.type}">${item.type}</h2>
-    <h2 class = "${item.gender}">${item.gender}</h2>
-    <h2 class = "canBeFound" id = "${item.canBeFound}">${item.canBeFound}</h2>
-  </div>`
+randomItems.forEach((item) =>
+  DomSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `<div class = "card" id = "">
+<h1 class = "name" id = "${item.name}">Name: ${item.name}</h1>
+<h2 class = "${item.type}">Type: ${item.type}</h2>
+<h2 class = "${item.gender}">Gender: ${item.gender}</h2>
+<h2 class = "canBeFound" id = "${item.canBeFound}">Can be found: ${item.canBeFound}</h2>
+</div>`
   )
 );
+
+//need to insert buttons
