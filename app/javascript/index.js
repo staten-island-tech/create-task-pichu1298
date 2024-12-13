7d9598432f72b1df22a4653f3f0e44f065facd7e
pichu1298 <62480135+pichu1298@users.noreply.github.com>
2024-12-13 12:33:15 -0500

    asd

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -5,6 +5,44 @@ const DomSelectors = {
   container: document.querySelector(".container"),
   buttonContainer: document.querySelector(".buttonContainer"),
 };
+const input = document.getElementById("input");
+const submitForm = document.getElementById("removeButton");
+let newItems = [];
+
+submitForm.addEventListener("click", (event) => {
+  event.preventDefault();
+  randomItems.forEach((item) => {
+    if (item.name === input.value) {
+      console.log(input.value);
+      inputNewItems();
+      //show every item that doesn't match input value
+    } else if (item.gender === input.value) {
+      console.log(input.value);
+      inputNewItems();
+    } else if (item.type === input.value) {
+      console.log(input.value);
+      inputNewItems();
+    } else if (item.canBeFound === input.value) {
+      console.log(input.value);
+      inputNewItems();
+    }
+  });
+});
+
+function inputNewItems(item) {
+  newItems = randomItems.filter(item != input.value);
+  newItems.forEach((item) =>
+    DomSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class = "card" id = "">
+  <h1 class = "name" id = "${item.name}">Name: ${item.name}</h1>
+  <h2 class = "${item.type}">Type: ${item.type}</h2>
+  <h2 class = "${item.gender}">Gender: ${item.gender}</h2>
+  <h2 class = "canBeFound" id = "${item.canBeFound}">Can be found: ${item.canBeFound}</h2>
+  </div>`
+    )
+  );
+}
 
 randomItems.forEach((item) =>
   DomSelectors.container.insertAdjacentHTML(
@@ -17,5 +55,3 @@ randomItems.forEach((item) =>
 </div>`
   )
 );
-
-//need to insert buttons
