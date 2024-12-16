ef29bf31263881326532de58b91c3f4927452f12
pichu1298 <62480135+pichu1298@users.noreply.github.com>
2024-12-16 12:32:19 -0500

    ads

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -3,7 +3,6 @@ import "../style/style.css";
 
 const DomSelectors = {
   container: document.querySelector(".container"),
-  buttonContainer: document.querySelector(".buttonContainer"),
 };
 const input = document.getElementById("input");
 const submitForm = document.getElementById("removeButton");
@@ -15,6 +14,7 @@ randomItems.forEach((item) =>
     "beforeend",
     `<div class = "card" id = "">
 <h1 class = "name" id = "${item.name}">Name: ${item.name}</h1>
+<img src="${item.imageUrl}" alt="">
 <h2 class = "${item.type}">Type: ${item.type}</h2>
 <h2 class = "${item.gender}">Gender: ${item.gender}</h2>
 <h2 class = "canBeFound" id = "${item.canBeFound}">Can be found: ${item.canBeFound}</h2>
@@ -25,12 +25,14 @@ randomItems.forEach((item) =>
 reset.addEventListener("click", (event) => {
   console.log("Reset button clicked!");
   DomSelectors.container.innerHTML = "";
+  newItems = [...randomItems];
   event.preventDefault();
   randomItems.forEach((item) =>
     DomSelectors.container.insertAdjacentHTML(
       "beforeend",
       `<div class = "card" id = "">
 <h1 class = "name" id = "${item.name}">Name: ${item.name}</h1>
+<img src="${item.imageUrl}" alt="">
 <h2 class = "${item.type}">Type: ${item.type}</h2>
 <h2 class = "${item.gender}">Gender: ${item.gender}</h2>
 <h2 class = "canBeFound" id = "${item.canBeFound}">Can be found: ${item.canBeFound}</h2>
@@ -41,6 +43,10 @@ reset.addEventListener("click", (event) => {
 
 submitForm.addEventListener("click", (event) => {
   event.preventDefault();
+  alogrithm();
+});
+
+function alogrithm() {
   randomItems.forEach((item) => {
     if (item.name === input.value) {
       console.log(input.value);
@@ -54,10 +60,9 @@ submitForm.addEventListener("click", (event) => {
     } else if (item.canBeFound === input.value) {
       console.log(input.value);
       inputNewItems(input.value);
-    } else {
     }
   });
-});
+}
 
 function inputNewItems(value) {
   DomSelectors.container.innerHTML = "";
@@ -73,6 +78,7 @@ function inputNewItems(value) {
       "beforeend",
       `<div class = "card" id = "">
   <h1 class = "name" id = "${item.name}">Name: ${item.name}</h1>
+  <img src="${item.imageUrl}" alt="">
   <h2 class = "${item.type}">Type: ${item.type}</h2>
   <h2 class = "${item.gender}">Gender: ${item.gender}</h2>
   <h2 class = "canBeFound" id = "${item.canBeFound}">Can be found: ${item.canBeFound}</h2>
